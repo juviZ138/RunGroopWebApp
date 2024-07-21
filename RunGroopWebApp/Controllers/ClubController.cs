937f519867c68f72894afdd2cd0045aeeb0130ec
juviZ138 <116240388+juviZ138@users.noreply.github.com>
2024-07-22 02:17:15 +0700

    Ceate Club

--- RunGroopWebApp/Controllers/ClubController.cs
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using RunGroopWebApp.Data;
 using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
 
@@ -11,21 +9,40 @@ namespace RunGroopWebApp.Controllers
         private readonly IClubRepository _clubRepository;
 
         // Call DataBase
-        public ClubController(IClubRepository clubRepository) {
+        public ClubController(IClubRepository clubRepository)
+        {
             _clubRepository = clubRepository;
         }
 
         public async Task<IActionResult> Index()
-        {    
+        {
             IEnumerable<Club> clubs = await _clubRepository.GetAll();
             return View(clubs);
         }
 
 
         // Details = 1, single
-        public async Task<IActionResult> Detail(int id) {
-            Club club = await _clubRepository.GetByIdAsync(id); 
+        public async Task<IActionResult> Detail(int id)
+        {
+            Club club = await _clubRepository.GetByIdAsync(id);
             return View(club);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(club);
+            }
+            _clubRepository.Add(club);
+            return RedirectToAction("Index");
+        }
+
     }
 }
